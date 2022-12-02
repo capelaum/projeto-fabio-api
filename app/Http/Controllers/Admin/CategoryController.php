5dbdc8bd774d98e51be7cacc723619bff4ad3719
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-12-02 10:55:03 -0300

    feat(admin): Show Category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\CategoryCollection;
+use App\Http\Resources\Admin\CategoryResource;
 use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    public function index()
+    /**
+     * @return CategoryCollection
+     */
+    public function index(): CategoryCollection
     {
         return new CategoryCollection(Category::all());
+    }
+
+    /**
+     * @param Category $category
+     * @return CategoryResource
+     */
+    public function show(Category $category): CategoryResource
+    {
+        return new CategoryResource($category);
     }
 }
